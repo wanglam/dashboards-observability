88d1f1ae3613316457e57590b0df7e2465bb0799
Lin Wang <wonglam@amazon.com>
2025-05-27 12:19:26 +0800

    Add explain in progress step detail
    Signed-off-by: Lin Wang <wonglam@amazon.com>

--- common/constants/ml_commons.ts
+++ common/constants/ml_commons.ts
@@ -7,6 +7,8 @@ const OBSERVABILITY_ML_COMMONS_API_PREFIX = '/api/observability/ml-commons';
 export const OBSERVABILITY_ML_COMMONS_API = {
   singleTask: `${OBSERVABILITY_ML_COMMONS_API_PREFIX}/tasks/{taskId}`,
   agents: `${OBSERVABILITY_ML_COMMONS_API_PREFIX}/agents`,
+  memory: `${OBSERVABILITY_ML_COMMONS_API_PREFIX}/memory`,
+  singleMemory: `${OBSERVABILITY_ML_COMMONS_API_PREFIX}/memory/{memoryId}`,
   memoryMessages: `${OBSERVABILITY_ML_COMMONS_API_PREFIX}/memory/{memoryId}/messages`,
   messageTraces: `${OBSERVABILITY_ML_COMMONS_API_PREFIX}/memory/message/{messageId}/traces`,
 };
@@ -16,6 +18,8 @@ const OPENSEARCH_ML_COMMONS_API_PREFIX = '/_plugins/_ml';
 export const OPENSEARCH_ML_COMMONS_API = {
   singleTask: `${OPENSEARCH_ML_COMMONS_API_PREFIX}/tasks/{taskId}`,
   agentsSearch: `${OPENSEARCH_ML_COMMONS_API_PREFIX}/agents/_search`,
+  memorySearch: `${OPENSEARCH_ML_COMMONS_API_PREFIX}/memory/_search`,
+  singleMemory: `${OPENSEARCH_ML_COMMONS_API_PREFIX}/memory/{memoryId}`,
   memoryMessages: `${OPENSEARCH_ML_COMMONS_API_PREFIX}/memory/{memoryId}/messages`,
   messageTraces: `${OPENSEARCH_ML_COMMONS_API_PREFIX}/memory/message/{messageId}/traces`,
 };
