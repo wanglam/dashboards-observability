e8c6e861135312a5f2452aa71321f50d9240ceec
Lin Wang <wonglam@amazon.com>
2025-05-29 17:42:05 +0800

    Optimize tool trace display
    Signed-off-by: Lin Wang <wonglam@amazon.com>

--- public/components/custom_panels/panel_modules/deep_research_container/message_trace_modal.tsx
+++ public/components/custom_panels/panel_modules/deep_research_container/message_trace_modal.tsx
@@ -47,20 +47,41 @@ export const MessageTraceModal = ({
   }, [messageId, http, dataSourceId]);
 
   const renderTraces = () => {
-    return traces.map(({ input, response, message_id: traceMessageId, origin }, index) => (
-      <React.Fragment key={traceMessageId}>
-        <EuiAccordion
-          id={`trace-${index}`}
-          buttonContent={`Step ${index + 1} - ${input}`}
-          paddingSize="l"
-        >
-          <EuiText className="wrapAll markdown-output-text" size="s">
-            {response ? <MarkdownRender source={response} /> : `No response from: ${origin}`}
-          </EuiText>
-        </EuiAccordion>
-        <EuiSpacer />
-      </React.Fragment>
-    ));
+    return traces.map(({ input, response, message_id: traceMessageId, origin }, index) => {
+      const isFromLLM = origin?.toLowerCase() === 'llm';
+      return (
+        <React.Fragment key={traceMessageId}>
+          <EuiAccordion
+            id={`trace-${index}`}
+            buttonContent={`Step ${index + 1} - ${isFromLLM ? input : `Execute ${origin}`}`}
+            paddingSize="l"
+          >
+            <EuiText className="wrapAll markdown-output-text" size="s">
+              {isFromLLM ? (
+                <MarkdownRender source={response} />
+              ) : (
+                <>
+                  <EuiAccordion
+                    id={`trace-step-${index}-input`}
+                    buttonContent={`${origin} input`}
+                    initialIsOpen
+                  >
+                    {input}
+                  </EuiAccordion>
+                  <EuiAccordion
+                    id={`trace-step-${index}-response`}
+                    buttonContent={`${origin} response`}
+                  >
+                    {response}
+                  </EuiAccordion>
+                </>
+              )}
+            </EuiText>
+          </EuiAccordion>
+          <EuiSpacer />
+        </React.Fragment>
+      );
+    });
   };
 
   return (
